7ff3038b3e4e7b5a0f11e932e6fea7bb04522c76
dhgk65gds <esjcstelitadr94@hotmail.com>
2024-05-10 19:27:45 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const isEven = (num) => num % 2 === 0;
+function longestConsecutive(nums) {
+  const set = new Set(nums);
+  let longest = 0;
+  for (const num of set) {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+      while (set.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+      longest = Math.max(longest, currentStreak);
+    }
+  }
+  return longest;
+}
