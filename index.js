262136d671024d851c4915953f202855f7406a15
dhgk65gds <esjcstelitadr94@hotmail.com>
2024-05-12 00:10:30 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function longestConsecutive(nums) {
-  const set = new Set(nums);
-  let longest = 0;
-  for (const num of set) {
-    if (!set.has(num - 1)) {
-      let currentNum = num;
-      let currentStreak = 1;
-      while (set.has(currentNum + 1)) {
-        currentNum++;
-        currentStreak++;
-      }
-      longest = Math.max(longest, currentStreak);
-    }
-  }
-  return longest;
+function getMax(arr) {
+  return Math.max(...arr);
 }
