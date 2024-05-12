cebf31db11a74a3a96be77734acf41a3e2089418
dhgk65gds <esjcstelitadr94@hotmail.com>
2024-05-12 14:20:14 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function getMax(arr) {
-  return Math.max(...arr);
+function reverseList(head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
 }
